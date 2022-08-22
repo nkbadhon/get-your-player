7976dac31d75ec218ff860789abb27015fab847c
nkbadhon <badhon7574@gmail.com>
2022-08-22 17:57:34 +0600

    get name done

--- javascript/calculation.js
+++ javascript/calculation.js
@@ -1,4 +1,39 @@
-function getname(fff) {
-    const playerid = document.getElementById("fff").parentNode.parentNode.childNodes[2].innerText
+document.getElementById('p1').addEventListener('click', function () {
+    const playerid = document.getElementById("p1").parentNode.parentNode.childNodes[2].innerText
     console.log(playerid);
-}
+})
+document.getElementById('p2').addEventListener('click', function () {
+    const playerid = document.getElementById("p2").parentNode.parentNode.childNodes[2].innerText
+    console.log(playerid);
+})
+document.getElementById('p3').addEventListener('click', function () {
+    const playerid = document.getElementById("p3").parentNode.parentNode.childNodes[2].innerText
+    console.log(playerid);
+})
+document.getElementById('p4').addEventListener('click', function () {
+    const playerid = document.getElementById("p4").parentNode.parentNode.childNodes[2].innerText
+    console.log(playerid);
+})
+document.getElementById('p5').addEventListener('click', function () {
+    const playerid = document.getElementById("p5").parentNode.parentNode.childNodes[2].innerText
+    console.log(playerid);
+})
+document.getElementById('p6').addEventListener('click', function () {
+    const playerid = document.getElementById("p6").parentNode.parentNode.childNodes[2].innerText
+    console.log(playerid);
+})
+document.getElementById('p7').addEventListener('click', function () {
+    const playerid = document.getElementById("p7").parentNode.parentNode.childNodes[2].innerText
+    console.log(playerid);
+})
+document.getElementById('p8').addEventListener('click', function () {
+    const playerid = document.getElementById("p8").parentNode.parentNode.childNodes[2].innerText
+    console.log(playerid);
+})
+document.getElementById('p9').addEventListener('click', function () {
+    const playerid = document.getElementById("p9").parentNode.parentNode.childNodes[2].innerText
+    console.log(playerid);
+})
+
+
+// Button press Count
