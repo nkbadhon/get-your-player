9b887d5798e7811f23cb72637b818f7ba50d1e13
nkbadhon <badhon7574@gmail.com>
2022-08-22 20:26:34 +0600

    name to the list updated

--- javascript/calculation.js
+++ javascript/calculation.js
@@ -1,39 +1,90 @@
+// Button press Count
+let x = 0;
+// if (x => 5) {
+//     document.getElementById('p1').disabled = true;
+//     document.getElementById('p2').disabled = true;
+//     document.getElementById('p3').disabled = true;
+//     document.getElementById('p4').disabled = true;
+//     document.getElementById('p5').disabled = true;
+//     document.getElementById('p6').disabled = true;
+//     document.getElementById('p7').disabled = true;
+//     document.getElementById('p8').disabled = true;
+//     document.getElementById('p9').disabled = true;
+// }
+
+
+// Get the player's name:
 document.getElementById('p1').addEventListener('click', function () {
     const playerid = document.getElementById("p1").parentNode.parentNode.childNodes[2].innerText
-    console.log(playerid);
+    const getList = document.getElementById('olist');
+    const li1 = document.createElement('li');
+    li1.innerText = playerid;
+    getList.appendChild(li1);
+    x++;
 })
 document.getElementById('p2').addEventListener('click', function () {
     const playerid = document.getElementById("p2").parentNode.parentNode.childNodes[2].innerText
-    console.log(playerid);
+    const getList = document.getElementById('olist');
+    const li1 = document.createElement('li');
+    li1.innerText = playerid;
+    getList.appendChild(li1);
+    x++;
 })
 document.getElementById('p3').addEventListener('click', function () {
     const playerid = document.getElementById("p3").parentNode.parentNode.childNodes[2].innerText
-    console.log(playerid);
+    const getList = document.getElementById('olist');
+    const li1 = document.createElement('li');
+    li1.innerText = playerid;
+    getList.appendChild(li1);
+    x++;
 })
 document.getElementById('p4').addEventListener('click', function () {
     const playerid = document.getElementById("p4").parentNode.parentNode.childNodes[2].innerText
-    console.log(playerid);
+    const getList = document.getElementById('olist');
+    const li1 = document.createElement('li');
+    li1.innerText = playerid;
+    getList.appendChild(li1);
+    x++;
 })
 document.getElementById('p5').addEventListener('click', function () {
     const playerid = document.getElementById("p5").parentNode.parentNode.childNodes[2].innerText
-    console.log(playerid);
+    const getList = document.getElementById('olist');
+    const li1 = document.createElement('li');
+    li1.innerText = playerid;
+    getList.appendChild(li1);
+    x++;
 })
 document.getElementById('p6').addEventListener('click', function () {
     const playerid = document.getElementById("p6").parentNode.parentNode.childNodes[2].innerText
-    console.log(playerid);
+    const getList = document.getElementById('olist');
+    const li1 = document.createElement('li');
+    li1.innerText = playerid;
+    getList.appendChild(li1);
+    x++;
 })
 document.getElementById('p7').addEventListener('click', function () {
     const playerid = document.getElementById("p7").parentNode.parentNode.childNodes[2].innerText
-    console.log(playerid);
+    const getList = document.getElementById('olist');
+    const li1 = document.createElement('li');
+    li1.innerText = playerid;
+    getList.appendChild(li1);
+    x++;
 })
 document.getElementById('p8').addEventListener('click', function () {
     const playerid = document.getElementById("p8").parentNode.parentNode.childNodes[2].innerText
-    console.log(playerid);
+    const getList = document.getElementById('olist');
+    const li1 = document.createElement('li');
+    li1.innerText = playerid;
+    getList.appendChild(li1);
+    x++;
 })
 document.getElementById('p9').addEventListener('click', function () {
     const playerid = document.getElementById("p9").parentNode.parentNode.childNodes[2].innerText
-    console.log(playerid);
+    const getList = document.getElementById('olist');
+    const li1 = document.createElement('li');
+    li1.innerText = playerid;
+    getList.appendChild(li1);
+    x++;
 })
 
 
-// Button press Count
